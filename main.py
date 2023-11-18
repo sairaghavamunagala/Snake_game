9e037266a2b6068de2e628e9c97a2d24c617e23d
Munagala Sai Raghava <sairaghava2001@gmail.com>
2023-11-18 22:21:29 +0530

    Chore: Add enums
    This commit adds enum to the direction class.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame
-
+from enum import Enum
 pygame.init()
 
+class Direction(Enum):
+    RIGHT=1
+    LEFT=2
+    UP=3
+    DOWN=4
 
 class SnakeGame:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.display=pygame.display.set_mode((self.width,self.height))
+        pygame.display.set_caption("Snake Game")
+        self.clock=pygame.time.Clock()
 
     def playstep(self):
         pass
